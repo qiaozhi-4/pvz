3ad9485ea53d769a8cdba65ea4d536de6361d364
乔治 <18890176107@163.com>
2025-08-23 00:23:53 +0800

    :art: 添加配置类

---
+++ src/Config.cpp
@@ -0,0 +1,5 @@
+/**
+ * @author XGeorge
+ * @date 2025/8/22 20:54:36
+*/
+#include "Config.h"
---
+++ src/Config.h
@@ -0,0 +1,18 @@
+/**
+ * @author XGeorge
+ * @date 2025/8/22 20:54:36
+*/
+#ifndef PVZ_CONFIG_H
+#define PVZ_CONFIG_H
+
+
+class Config
+{
+public:
+    inline static bool showMenuBar = false;          // 显示窗口菜单栏
+    inline static bool showMainMenuBar = false;          // 显示窗口菜单栏
+
+};
+
+
+#endif //PVZ_CONFIG_H
